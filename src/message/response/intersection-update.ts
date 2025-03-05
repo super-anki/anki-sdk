e5bc09ae75b2533a139c6410af11df968c9f853a
Vincent Talbot <vincent@talbot.ninja>
2025-03-05 09:28:00 -0500

    feat: add intersection update response

---
+++ src/message/response/intersection-update.ts
@@ -0,0 +1,21 @@
+import { Message } from "../message"
+
+export class IntersectionUpdateResponse extends Message {
+  public readonly roadPieceId: number
+  public readonly offsetRoadCenter: number
+  public readonly intersectionCode: number
+  public readonly isExisting: number
+  public readonly mmSinceLastTransitionBar: number
+  public readonly mmSinceLastIntersectionCode: number
+
+  public constructor(id: string, payload: Buffer) {
+    super(id, payload)
+
+    this.roadPieceId = this.payload.readUInt8(2)
+    this.offsetRoadCenter = this.payload.readFloatLE(3)
+    this.intersectionCode = this.payload.readUInt8(7)
+    this.isExisting = this.payload.readUInt8(8)
+    this.mmSinceLastTransitionBar = this.payload.readUInt16LE(9)
+    this.mmSinceLastIntersectionCode = this.payload.readUInt16LE(11)
+  }
+}
