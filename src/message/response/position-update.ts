5d6287f94f5916105ef836ffba33cfebc5c9297d
Vincent Talbot <vincent@talbot.ninja>
2025-03-05 09:28:01 -0500

    feat: add position update response

---
+++ src/message/response/position-update.ts
@@ -0,0 +1,27 @@
+import { Message } from "../message"
+
+export class PositionUpdateResponse extends Message {
+  public readonly locationId: number
+  public readonly roadPieceId: number
+  public readonly offsetRoadCenter: number
+  public readonly speed: number
+  public readonly parsingFlags: number
+  public readonly lastReceiveLaneChangeCommandId: number
+  public readonly lastExecLangeChangeCommandId: number
+  public readonly lastDesiredLaneChangeSpeed: number
+  public readonly lastDesiredSpeed: number
+    
+  public constructor(id: string, payload: Buffer) {
+    super(id, payload)
+
+    this.locationId = this.payload.readUInt8(2)
+    this.roadPieceId = this.payload.readUInt8(3)
+    this.offsetRoadCenter = this.payload.readFloatLE(4)
+    this.speed = this.payload.readUInt16LE(8)
+    this.parsingFlags = this.payload.readUInt8(10)
+    this.lastReceiveLaneChangeCommandId = this.payload.readUInt8(11)
+    this.lastExecLangeChangeCommandId = this.payload.readUInt8(12)
+    this.lastDesiredLaneChangeSpeed = this.payload.readUInt16LE(13)
+    this.lastDesiredSpeed = this.payload.readUInt16LE(15)
+  }
+}
